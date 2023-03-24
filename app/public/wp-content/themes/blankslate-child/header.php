242e110e3116a8129fdec6f3b4a38f89695a35e5
destiinova <contactpro.deborah@gmail.com>
2023-03-24 20:56:09 +0100

    header

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
+        <div id="wrapper" class="hfeed">
+    <header id="header" role="banner">
+        <div id="branding">
+        <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 
 <?php
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
