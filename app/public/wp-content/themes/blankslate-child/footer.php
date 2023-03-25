7bc56b8afcb5b2b60ea42765cf23d69a52f6322a
destiinova <contactpro.deborah@gmail.com>
2023-03-25 11:04:25 +0100

    header & footer

--- app/public/wp-content/themes/blankslate-child/footer.php
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -3,6 +3,9 @@
         </div>
                 <footer id="footer" role="contentinfo">
                     <div id="copyright">
+                        <ul>
+                        <li><a href="/accueil/">Mentions légales</a></li>
+                        </ul>
                     &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
                     </div>
                 </footer>
--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
     <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@500&display=swap" rel="stylesheet">
+    
+    
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width" />
         <?php wp_head(); ?>
     </head>
@@ -9,6 +14,11 @@
         <?php wp_body_open(); ?>
         <div id="wrapper" class="hfeed">
     <header id="header" role="banner">
+
+
+    <div><img src="img/Logo.png" alt="Logo"></div>
+
+
         <div id="branding">
         <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 
@@ -25,7 +35,7 @@ if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1
 
 <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
+
 </nav>
 </header>
 <div id="container">
