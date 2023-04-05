cfa3715d8d5cc5901926ce2dcf80fc0e2c60b2c6
destiinova <contactpro.deborah@gmail.com>
2023-04-05 16:04:23 +0200

    MAJ pages

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -16,25 +16,24 @@
     <header id="header" role="banner">
 
 
-    <div><img src="logo.png" alt="Logo"></div>
-
-
         <div id="branding">
         <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 
-<?php
+<!-- <?php
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
 echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
+?> -->
+
 
 
 </div>
 <div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
 </div>
 
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+    <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+    <div><img src="logo.png" alt="Logo"></div>
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
 
 </nav>
     </header>
